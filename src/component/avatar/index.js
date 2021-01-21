e5570f7ba62f2f6b31e0e087d1d0ccbb8f552541
yangpan <yangpan@yangpandeMacBook-Pro.local>
2021-01-21 17:30:13 +0800

    v1.0.2

--- src/component/avatar/index.js
+++ src/component/avatar/index.js
@@ -4,10 +4,27 @@ import appContext from '../../appContext'
 import './index.scss'
 
 export default function Avatar({ data = {} }) {
+  const [image, setImage] = React.useState('')
+    function toDataURL(url, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+          callback(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+      };
+      xhr.open('GET', url);
+      xhr.responseType = 'blob';
+      xhr.send();
+  }
 
-  const {dispatch, modules} = React.useContext(appContext)
-  console.log(dispatch, modules)
+  React.useEffect(() => {
+    toDataURL(data.image, function(dataUrl) {
+      setImage(dataUrl)
+    })
+  }, [data])
   return (<div className="avatar">
-      <img src={data.image} />
+      <img src={image} />
     </div>)
 }
